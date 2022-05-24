9e1cc211bd65c1590cbfd4784ab9c9dc84623412
Patrick Redman <predman@estalea.com>
2022-05-24 08:04:15 +0200

    address multithreading / thread safety of round robin strategy and add tests

--- src/main/java/org/predman/iptiq/RoundRobinStrategy.java
+++ src/main/java/org/predman/iptiq/RoundRobinStrategy.java
@@ -1,23 +1,24 @@
 package org.predman.iptiq;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class RoundRobinStrategy implements LoadBalancerStrategy {
     
     private final ProviderRegistry providerRegistry;
-    private int counter;
+    private AtomicInteger counter;
     
     public RoundRobinStrategy(ProviderRegistry providerRegistry) {
         this.providerRegistry = providerRegistry;
-        counter = 0;
+        counter = new AtomicInteger(0);
     }
     
     @Override
     public Provider nextProvider() {
         
         List<String> ids = providerRegistry.getActiveIds();
-        int index = counter++ % ids.size();
+        int index = counter.getAndIncrement() % ids.size();
         String id = ids.get(index);
         return providerRegistry.get(id);
     }
--- src/test/java/org/predman/iptiq/RoundRobinStrategyTest.java
+++ src/test/java/org/predman/iptiq/RoundRobinStrategyTest.java
@@ -1,26 +1,32 @@
 package org.predman.iptiq;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class RoundRobinStrategyTest {
     
-    private RoundRobinStrategy strategy;
-    
-    @BeforeEach
-    public void beforeEach() {
-        // NOTE: would typically use a mock / stub here
-        ProviderRegistry providerRegistry = new ProviderRegistry();
-        for (int i = 0; i < 3; i++) {
-            providerRegistry.register(new Provider());
-        }
-        strategy = new RoundRobinStrategy(providerRegistry);
-    }
-    
     @Test
     public void next() {
-        Assertions.assertNotNull(strategy.nextProvider());
+        // setup a few providers
+        Provider[] providers = new Provider[] {
+                new Provider(),
+                new Provider(),
+                new Provider()
+        };
+        ProviderRegistry registry = new ProviderRegistry();
+        for (int i = 0; i < providers.length; i++) {
+            registry.register(providers[i]);
+        }
+        
+        RoundRobinStrategy roundRobin = new RoundRobinStrategy(registry);
+        
+        Assertions.assertEquals(providers[0], roundRobin.nextProvider());
+        Assertions.assertEquals(providers[1], roundRobin.nextProvider());
+        Assertions.assertEquals(providers[2], roundRobin.nextProvider());
+        Assertions.assertEquals(providers[0], roundRobin.nextProvider());
+        Assertions.assertEquals(providers[1], roundRobin.nextProvider());
+        Assertions.assertEquals(providers[2], roundRobin.nextProvider());
+        
     }
 }
