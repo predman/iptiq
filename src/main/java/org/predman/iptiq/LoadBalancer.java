e8a6ad3fbe70e6f3179b0c7950d173f1756188d4
Patrick Redman <predman@estalea.com>
2022-05-24 08:48:50 +0200

    add in max requests bits

--- src/main/java/org/predman/iptiq/LoadBalancer.java
+++ src/main/java/org/predman/iptiq/LoadBalancer.java
@@ -1,8 +1,14 @@
 package org.predman.iptiq;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+
 public class LoadBalancer {
     
+    private static final int MAX_PARALLEL_REQUESTS_PER_PROVIDER = 5;
+    
+    private AtomicInteger requestCount;
     private final ProviderRegistry providerRegistry;
     private final LoadBalancerStrategy strategy;
     
@@ -13,6 +19,9 @@ public class LoadBalancer {
         //        strategy = new RandomStrategy(providerRegistry);
         strategy = new RoundRobinStrategy(providerRegistry);
         
+        // initialize the request counter to zero (since no requests yet on startup)
+        requestCount = new AtomicInteger(0);
+        
         // start the heartbeat monitor
         new HeartBeatMonitor(providerRegistry).start();
     }
@@ -22,6 +31,25 @@ public class LoadBalancer {
     }
     
     public String get() {
-        return strategy.nextProvider().get();
+        // NOTE: 'null' here represents something like an http 503 unavailable response
+        String value = null;
+        int activeProviders = providerRegistry.getActiveIds().size();
+        /*
+        Here we increment so that we 'acquire' the next available request, then check if over the max (rather than get + check then increment).
+        We always decrement regardless of whether we were able to proceed with the request or not.
+         */
+        if (requestCount.incrementAndGet() <= activeProviders * MAX_PARALLEL_REQUESTS_PER_PROVIDER) {
+            value = doGet();
+        }
+        requestCount.decrementAndGet();
+        return value;
+    }
+    
+    private String doGet() {
+        Provider provider = strategy.nextProvider();
+        if (provider == null) {
+            return null;
+        }
+        return provider.get();
     }
 }
